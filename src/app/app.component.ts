e5e2d64d554bd4b7b9b70f0272eec90dae03f143
mohan <kmohan0910@gmail.com>
2024-10-21 23:02:56 +0530

    Updated Container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,49 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'draganddrop';
+  ngOnInit() {
+    let newX = 0,
+      newY = 0,
+      startX = 0,
+      startY = 0;
+
+    const card: any = document.getElementById('card');
+
+    card.addEventListener('mousedown', mouseDown);
+
+    function mouseDown(e: MouseEvent) {
+      startX = e.clientX;
+      startY = e.clientY;
+
+      document.addEventListener('mousemove', mouseMove);
+      document.addEventListener('mouseup', mouseUp);
+    }
+
+    function mouseMove(e: MouseEvent) {
+      newX = startX - e.clientX;
+      newY = startY - e.clientY;
+
+      startX = e.clientX;
+      startY = e.clientY;
+
+      card.style.top = Math.max(0, card.offsetTop) - newY + 'px';
+      card.style.left =
+        Math.min(
+          Math.max(0, card.offsetLeft),
+          card.parentElement.offsetWidth - card.offsetWidth
+        ) -
+        newX +
+        'px';
+
+      console.log(card.offsetTop, card.offsetRight);
+    }
+
+    function mouseUp(e: MouseEvent) {
+      document.removeEventListener('mousemove', mouseMove);
+    }
+  }
 }
