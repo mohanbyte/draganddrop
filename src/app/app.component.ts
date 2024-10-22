54ef6bb4b1fd12f15a7ce6267d6101c2f999fe41
mohan <kmohan0910@gmail.com>
2024-10-22 21:33:22 +0530

    Refactor card positioning logic and update container layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,64 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'draganddrop';
-  ngOnInit() {
-    let newX = 0,
-      newY = 0,
-      startX = 0,
-      startY = 0;
+  cards = [1, 2];
+  ngAfterViewInit() {
+    for (let element of this.cards) {
+      let newX = 0,
+        newY = 0,
+        startX = 0,
+        startY = 0;
+      console.log(element);
+      const card: any = document.getElementById('card-' + element);
 
-    const card: any = document.getElementById('card');
+      card.addEventListener('mousedown', mouseDown);
 
-    card.addEventListener('mousedown', mouseDown);
+      function mouseDown(e: MouseEvent) {
+        startX = e.clientX;
+        startY = e.clientY;
 
-    function mouseDown(e: MouseEvent) {
-      startX = e.clientX;
-      startY = e.clientY;
+        document.addEventListener('mousemove', mouseMove);
+        document.addEventListener('mouseup', mouseUp);
+      }
 
-      document.addEventListener('mousemove', mouseMove);
-      document.addEventListener('mouseup', mouseUp);
-    }
+      function mouseMove(e: MouseEvent) {
+        newX = startX - e.clientX;
+        newY = startY - e.clientY;
 
-    function mouseMove(e: MouseEvent) {
-      newX = startX - e.clientX;
-      newY = startY - e.clientY;
+        startX = e.clientX;
+        startY = e.clientY;
 
-      startX = e.clientX;
-      startY = e.clientY;
+        card.style.top =
+          Math.min(
+            Math.max(0, card.offsetTop),
+            card.parentElement.offsetHeight - card.offsetHeight
+          ) -
+          newY +
+          'px';
+        card.style.left =
+          Math.min(
+            Math.max(0, card.offsetLeft),
+            card.parentElement.offsetWidth - card.offsetWidth
+          ) -
+          newX +
+          'px';
 
-      card.style.top = Math.max(0, card.offsetTop) - newY + 'px';
-      card.style.left =
-        Math.min(
-          Math.max(0, card.offsetLeft),
-          card.parentElement.offsetWidth - card.offsetWidth
-        ) -
-        newX +
-        'px';
+        console.log(card.offsetTop, card.offsetRight);
+      }
 
-      console.log(card.offsetTop, card.offsetRight);
-    }
-
-    function mouseUp(e: MouseEvent) {
-      document.removeEventListener('mousemove', mouseMove);
+      function mouseUp(e: MouseEvent) {
+        document.removeEventListener('mousemove', mouseMove);
+      }
     }
   }
 }
